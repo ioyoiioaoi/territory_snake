6145b58ed7329bf33355b8c2dc11fe8bcc6630f8
IOYOI <70329737+ioyoiioaoi@users.noreply.github.com>
2025-11-22 03:53:14 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,10 +2,21 @@ let game;
 
 window.onload = () => {
     game = new Game('gameCanvas');
+
+    // Setup start button
+    const startBtn = document.getElementById('start-btn');
+    if (startBtn) {
+        startBtn.addEventListener('click', showFactionSelect);
+    }
 };
 
 function startGame(factionKey) {
     if (game) {
         game.start(factionKey);
     }
+}
+
+function showFactionSelect() {
+    document.getElementById('rules-screen').classList.add('hidden');
+    document.getElementById('start-screen').classList.remove('hidden');
 }
